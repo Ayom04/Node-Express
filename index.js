71aa2e222e6a54927e7d8d564d74f06b0601262d
Ayom04 <abdullahayomide04@gmail.com>
2023-05-21 22:37:17 +0100

    working with local apis

--- index.js
+++ index.js
@@ -1,130 +1,69 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+const data = require('./data')
+const PORT = 1432
 
-const PORT = 4589;
-const data = [
-    {
-        id: 1,
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        address: "7 Agunbiade street, Jakarta",
-        phone: "08123456789",
-        
-    },
-    {
-        id: 2,
-        name: "Ayodeji Ayorinde",
-        email: "ayokele@gmail.com",
-        address: "11, Wonuola street, Akoka",
-        phone: "09123456789",
-        
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        status: 'OK',
+        message: "welcome to my api"
+    })
+})
+app.post('/user/login', (req, res) => {
+    const {email, password} = req.body
+    if(!email || !password){
+        res.status(200).json({
+            message: "Invalid credentials"
+        })
     }
-]
-app.get("/", (req, res) => {
-    res.json({
-        status: "success",
-        message: "Welcome to my API"
+    const checkUser= data.find(data => data.email === email && data.password === password)
+    if(!checkUser){
+        res.status(200).json({
+            message: "Invalid credentials"
+        })
+    }
+    res.status(200).json({
+        message: "Login successful",
+        data: checkUser
     })
 })
-app.post("/", (req, res) =>{
-    res.json({
-        status: "success",
-        message: "logging in!"
+app.get('/user/id/:email',(req,res)=>{
+    const {email} = req.params
+    res.status(200).json({
+        data: email
     })
 })
-app.get("/users", (req, res) =>{
-    res.json({
-        status: "success",
-        meesage: "logged in",
-        data: data
-    })
-})
-app.post("/user/add", (req, res) =>{
-    
-    const {name, email, address, phone} = req.body
-    if (!name || !email || !phone || !address || email.indexOf("@") === -1){
-        res.json({
-            status: "fail",
-            message: "All field are required"
+app.post('/user/signup',(req,res)=>{
+    const {name,email,password, age} = req.body
+    if(!name ||!email ||!password ||!age){
+        res.status(200).json({
+            message: "Invalid credentials"
         })
     }
     const tempData = {
-        id: data.length + 1,
+        id: data.length++,
         name,
         email,
-        address,
-        phone,
-
+        password,
+        age
     }
+    
+    
+
     data.push(tempData)
     res.json({
-        status: 'success',
-        message: "Data created successfully"
-    })
-})
-app.put('/user/update', (req,res)=>{
-    const {id, name, email, address, phone} = req.body
-    if (!id ||!name ||!email ||!phone ||!address || email.indexOf("@") === -1){
-        res.json({
-                    status: "fail",
-                    message: "All field are required"
-                })
-    }
-    const filteredData = data.filter(item => item.id === id)
-
-    filteredData[0].name = name
-    filteredData[0].phone = phone
-    filteredData[0].email = email
-    filteredData[0].address = address
-    res.json({
-        status:'success',
-        message: "Data updated successfully",
+        message: "Signup successful",
         data
     })
 })
-app.delete("/user/delete", (req,res) =>{
-    const {id} = req.body
-    if (!id) {
-        res.json({
-            status: "fail",
-            message: "id is  required"
-        })
-    }
-    data.forEach(item => {
-        if (item.id === id) {
-            data.splice(id, 1)
-        }
-
-        })
-})
-app.get("/user/profile/:email", (req,res)=> {
-    const {email} = req.params
-    if(!email || email.indexOf('@') === -1) {
-        res.json({
-            status: "fail",
-            message: "email is required"
-        })
-    }
-    res.json({
-        statusbar: "success",
-        message: email
+app.get('/users', (req, res) => {
+    res.status(200).json({
+        data: data
     })
 })
-app.get('/user/saving/',(req,res)=>{
-    const {token} = req.query
-    if(!token) {
-        res.json({
-            status: "fail",
-            message: "token is required"
-        })
-    }
-    res.json({
-        statusbar: "success",
-        message: token
-    })
-})
-app.listen(PORT, (req,res)=>{
-    console.log(`Example app listening on port ${PORT}`)
+app.listen(PORT, (req, res)=> {
+    console.log(`listening on port ${PORT}`)
 })
